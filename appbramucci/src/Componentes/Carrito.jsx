f55b968c97a2487acfed747513eccf08043700d0
German Bramuccci <germanbramucci@gmail.com>
2022-03-03 20:58:34 -0300

    entrega landing

---
+++ appbramucci/src/Componentes/Carrito.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import carro from '../carrito-de-compras.png'
+
+function Carrito(){
+    return<>
+        <a href=""><img src={carro} alt="" /></a>
+    </>
+}
+
+export default Carrito
